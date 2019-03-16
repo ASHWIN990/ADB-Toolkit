f7695e9192be5a18ed84b552df4cca39197c4ab4
Ashwini Sahu <46531635+ASHWIN990@users.noreply.github.com>
2019-03-16 12:38:45 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ $(id -u) -ne 0 ]; then
+        echo -e "\e[1;91mTHE INSTALLATION SCRIPT MUST BE RAN AS ROOT\e[0m"
+        exit 1
+fi
+sudo apt-get update
+sudo apt-get install adb -y
+sudo apt-get install fastboot -y
+chmod +x ADB-Toolkit.sh
+clear
+
+echo -e "\e[1;92mINSTALLATION COMPLETED"
+echo
+echo -e "\e[1;92mUSAGE = \e[1;91m./ADB-Toolkit.sh\e[0m"
+echo
+echo
