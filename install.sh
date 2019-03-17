ff7461da0a0198af592b0387c5df83f06e27f24b
Ashwini Sahu <46531635+ASHWIN990@users.noreply.github.com>
2019-03-17 17:59:42 +0530

    Installation Script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 if [ $(id -u) -ne 0 ]; then
         echo -e "\e[1;91mTHE INSTALLATION SCRIPT MUST BE RAN AS ROOT\e[0m"
         exit 1
@@ -8,7 +9,7 @@ sudo apt-get install adb -y
 sudo apt-get install fastboot -y
 chmod +x ADB-Toolkit.sh
 clear
-
+echo
 echo -e "\e[1;92mINSTALLATION COMPLETED"
 echo
 echo -e "\e[1;92mUSAGE = \e[1;91m./ADB-Toolkit.sh\e[0m"
