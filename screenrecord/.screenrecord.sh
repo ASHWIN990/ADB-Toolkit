6d317ca97d89319cb7057525048e8a77509cc82f
Ashwini Sahu <46531635+ASHWIN990@users.noreply.github.com>
2020-04-02 16:40:38 +0530

    Update .screenrecord.sh

--- screenrecord/.screenrecord.sh
+++ screenrecord/.screenrecord.sh
@@ -1,47 +1,30 @@
 #!/bin/bash
 
-
 set -euo pipefail
 IFS=$'\n\t'
 
-function usage(){
-    echo -e " "
-    exit 1
-}
-
-function checkIfFileExists(){
-    if [ -f ${FILENAME} ]; then
-        echo -e "\e[1;91m[ERROR] \e[1;93mThe recording you are trying to create already exists!"
-        read -r -p $'\e[1;93mDo you want to overwrite this file? [y/N] ' RESPONSE
-        RESPONSE=`echo ${RESPONSE}| awk '{print tolower($0)}'`
-        if [[ (${RESPONSE} == "" || ${RESPONSE} == "n" || ${RESPONSE} == "no") ]];then
-            exit 1
-        fi
-    fi
-}
-
 function recordScreen(){
-	echo -e "\e[1;92mRecording '${FILENAME}'..."
+	echo -e "\e[1;92mRecordingthe screen to '${FILENAME}'...\n\e[91m"
 	adb shell screenrecord /sdcard/${FILENAME} &
 	_PID=$!
 
-	read -r -p $'\e[1;93mPress \e[1;91m[Enter] \e[1;93mto stop recording...'
-	kill ${_PID}
+	read -r -p $'\e[1;91mPress \e[1;91m[Enter] \e[1;93mto stop recording...'
+	kill ${_PID} > /dev/null 2&>1
 
-	echo -e "\e[1;92mStopping recording..."
-	while kill -0 "${_PID}"; do
-        sleep 0.5
-    done
+	echo -e "\n\e[1;92mStopping recording...\n"
+    sleep 0.5
 
-	echo -e "\e[1;92mDownloading recording..."
+	echo -e "\e[1;92mPulling recording from device...\n\e[91m"
 	adb pull /sdcard/${FILENAME} screenrecord/${FILENAME}
+    echo -e "\n\e[1;92mProcess complete.\n"
+	
+    echo -e "\e[1;93mDeleting screenrecord '${FILENAME}' from device...\n\e[91m"
 	adb shell rm /sdcard/${FILENAME}
-
 	echo -e "\e[1;92mProcess complete."
 	exit 0
 }
 
-[[ $# -eq 1 ]] || { usage; }
+[[ $# -eq 1 ]] || { echo; }
 
 for _ARGUMENT in "$@"
 do
@@ -59,5 +42,4 @@ do
     esac
 done
 
-checkIfFileExists
 recordScreen
