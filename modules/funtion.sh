14e2220a18711f2382b494e77649b4baf41fece3
Ashwini Sahu <46531635+ASHWIN990@users.noreply.github.com>
2020-04-13 21:01:40 +0530

    UPDATED TO VERSION 2.32

--- modules/funtion.sh
+++ modules/funtion.sh
@@ -63,7 +63,7 @@ echo -e "\e[1;93m19.  \e[1;92mCOPY ALL THE CAMERA PHOTOS"
 echo -e "\e[1;93m20.  \e[1;92mCOPY ALL THE DOWNLOADS"
 echo -e "\e[1;93m21.  \e[1;92mCOPY ALL WHATSAPP DATA"
 echo -e "\e[1;93m22.  \e[1;92mCOPY ALL DEVICE STORAGE ( Takes time )           \e[1;93mA.  \e[1;92mABOUT AUTHOUR "
-echo -e "\e[1;93m23.  \e[1;92mCOPY A SPECIFIED FILE OR FOLDER                  \e[1;93mE.  \e[1;92mEXIT or press Ctrl+c\n"
+echo -e "\e[1;93m23.  \e[1;92mCOPY A SPECIFIED FILE OR FOLDER                  \e[1;91mEXIT or press Ctrl+c\n"
 echo -e $revised
 read -p $'\e[1;4;91mSELECT ONE OF THE OPTIONS WITH THE RESPECTED NUMBER\e[0m\e[24;1;97m : ' options
  
@@ -112,8 +112,6 @@ case $options in
   "29") echo -e; bash modules/opt29;;
   "A") echo -e; bash modules/about;;
   "a") echo -e; bash modules/about;;
-  "E") clean_exit; exit;;
-  "e") clean_exit; exit;;
 
   *) clear; revised="\e[1;4;91mENTER THE RIGHT OPTION BRO :-)\n"; option_list;;
 
